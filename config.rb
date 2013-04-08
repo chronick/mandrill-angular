b68ef4295b011655f9322226dff6ee2965e93c85
Nick Donohue <ndonohue@gmail.com>
2013-04-08 19:27:11 -0400

    relative assets

--- config.rb
+++ config.rb
@@ -110,7 +110,7 @@ configure :build do
   # activate :cache_buster
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Compress PNGs after build
   # First: gem install middleman-smusher
